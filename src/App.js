ec546e6f64a3a459e7c1f2b8fde4545826cda91e
Akhil Nayak <aakhiljay06@gmail.com>
2023-07-03 14:37:57 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     Akhil Nk 😊 personal portfolio Under Construction..
+    </div>
+  );
+}
+
+export default App;
